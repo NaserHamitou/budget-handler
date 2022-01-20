22289355e534a86f11ff70a4e876117e67d4482c
Nas <naserhamitou98@gmail.com>
2022-01-19 21:12:10 -0500

    main page UI set up

---
+++ budget-handler/src/components/item-component.js
@@ -0,0 +1,35 @@
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
+import { currencyFormater } from "../utils";
+
+export default function Item({ name, amount, maxAmount }) {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+                    <div className="me-2">{name}</div>
+                    <div className="d-flex align-items-baseline">{currencyFormater.format(amount)}
+                        <span className="text-muted fs-6 ms-1">/ {currencyFormater.format(maxAmount)}</span>
+                    </div>
+                </Card.Title>
+                <ProgressBar 
+                    className="rounded-pill" 
+                    variant={setProgressBar(amount, maxAmount)}
+                    min={0}
+                    max={maxAmount}
+                    now={amount} />
+                    <Stack direction="horizontal" gap="2" className="mt-4">
+                        <Button variant="outline-primary" className="ms-auto">Add Expense</Button>
+                        <Button variant="outline-secondary">Expend ✚</Button>
+                    </Stack>
+            </Card.Body>
+        </Card>
+    );
+}
+
+const setProgressBar = (amount, max) => {
+    const ratio = amount/max;
+    if ( ratio < 0.5) return "primary"
+    if ( ratio < 0.75) return "warning"
+    return "danger"
+
+}
